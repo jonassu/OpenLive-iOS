94fad423e41e987d204507d704a204dbf16d7b51
苏乐雨 <suleyu@qq.com>
2017-10-16 14:39:36 +0800

    Support Broadcast video second opening

--- OpenLive/LiveRoomViewController.swift
+++ OpenLive/LiveRoomViewController.swift
@@ -241,7 +241,7 @@ private extension LiveRoomViewController {
 }
 
 extension LiveRoomViewController: AgoraRtcEngineDelegate {
-    func rtcEngine(_ engine: AgoraRtcEngineKit!, firstRemoteVideoDecodedOfUid uid: UInt, size: CGSize, elapsed: Int) {
+    func rtcEngine(_ engine: AgoraRtcEngineKit!, didJoinedOfUid uid: UInt, elapsed: Int) {
         let userSession = videoSession(ofUid: Int64(uid))
         rtcEngine.setupRemoteVideo(userSession.canvas)
     }
